e8443e91db32cf359e380a0456c70ce7eb398a5b
yaser <yaser@pasaportestudio.com>
2021-06-01 10:30:36 -0500

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	mode: 'jit',
+	purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				lg: '2rem',
+				xl: '6rem',
+				'2xl': '12rem',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
